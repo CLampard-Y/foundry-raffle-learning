21b412639e9efd3c503c100155c65424a876b0ac
CLampard-Y-HK <petwosagannn@163.com>
2026-07-21 22:48:15 +0800

    feat(deploy): add VRF subscription creation flow
    - add an interaction script for creating VRF subscriptions
    - resolve the coordinator from HelperConfig for standalone execution
    - create a subscription during raffle deployment when no ID is configured

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateSubscription} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle, HelperConfig) {
@@ -12,28 +13,17 @@ contract DeployRaffle is Script {
     }
 
     function deployContract() internal returns (Raffle, HelperConfig) {
-        /**
-         * uint256 entranceFee;
-         * uint256 interval;
-         * address vrfCoordinator;
-         * bytes32 gasLane;
-         * uint256 subscriptionId;
-         * uint32 callbackGasLimit;
-         *
-         */
-
         HelperConfig helperConfig = new HelperConfig();
-
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        if (config.subscriptionId == 0) {
+            CreateSubscription createSubscription = new CreateSubscription();
+            (config.subscriptionId, config.vrfCoordinator) =
+                createSubscription.createSubscription(config.vrfCoordinator);
+        }
+
         vm.startBroadcast();
-        Raffle raffle = new Raffle(
-            config.entranceFee,
-            config.interval,
-            config.vrfCoordinator,
-            config.gasLane,
-            config.subscriptionId,
-            config.callbackGasLimit
-        );
+        Raffle raffle = new Raffle(config.entranceFee, config.interval, config.vrfCoordinator, config.gasLane, config.subscriptionId, config.callbackGasLimit);
         vm.stopBroadcast();
 
         return (raffle, helperConfig);
